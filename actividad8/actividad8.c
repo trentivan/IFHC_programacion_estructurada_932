ef8662a5cb2d9d9757118ddda6bb0a91d566bd88
trentivan <141792967+trentivan@users.noreply.github.com>
2023-10-01 20:30:55 -0700

    modified:   actividad8/actividad8.c 	modified:   actividad8/output/actividad8.exe

--- actividad8/actividad8.c
+++ actividad8/actividad8.c
@@ -5,18 +5,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void menu(void);
 int msg(void);
 int reiniciar(void);
-int validar(char [], int , int ); 
+int validar(char [], int li, int ls); 
 
-void vectorManual(int []);
-void vectorAleatorio(int []);
-void vectores(int [], int [], int []);
-void imprimirVectores(int [], int [], int []);
-void llenarMatriz(int [], int [], int [][3]);
-void imprimirMattriz(int [][3]);
+void vectorManual(int vector1[], int n);
+void vectorAleatorio(int vector2[], int n);
+void vectores(int vector1[], int vector2[], int vector3[], int n);
+void imprimirVectores(int *vector1, int n);
+void llenarMatriz(int *vector1, int *vector2, int matriz[][3]);
+void imprimirMatriz(int matriz[][3], int m);
 
 int main()
 {
@@ -26,39 +27,55 @@ int main()
 
 void menu()
 {
-    int op, n=10, n2;
+    int op;
+    int n=10;
+    int m=4;
+    int vector1[n];
+    int vector2[n];
+    int vector3[n+n];
+    int matriz[m][m];
+    // n=10;
     do
     {
+    //     fflush(stdin);
+    //     gets(op);
+        // int vector1[n];
+        // int vector2[n];
+        // int vector3[n+n];
+        // int matriz[3][3];
         fflush(stdin);
         op= msg();
-        int vector1[n];
-        int vector2[n];
-        int vector3[n];
-        int matriz[n2][n2];
         switch(op)
         {
+            // fflush(stdin);
+            // op= msg();
             case 1:
-            vectorManual(vector1);
+            vectorManual(vector1, n);
             break;
 
             case 2:
-            vectorAleatorio(vector2);
+            vectorAleatorio(vector2, n);
             break;
 
             case 3:
-            vectores(vector1, vector2, vector3);
+            vectores(vector1, vector2, vector3, n);
             break;
 
             case 4:
-            imprimirVectores(vector1, vector2, vector3);
+            printf("vector 1 (llenado manualmente)\n");
+            imprimirVectores(vector1, 10);
+            printf("vector 2 llenado aleatoriamente\n");
+            imprimirVectores(vector2, 10);
+            printf("vector 3 (llenado a base de los vectores 1 y 2)\n");
+            imprimirVectores(vector3, 20);
             break;
 
             case 5:
-            llenarMatriz(vector1, vector2, matriz[3][3]);
+            llenarMatriz(vector1, vector2, matriz);
             break;
 
             case 6:
-            imprimirMattriz(matriz[][n2]);
+            imprimirMatriz(matriz, m);
             break;
         }
 
@@ -73,45 +90,95 @@ int msg()
     printf("2) llenarvector automaticamente.\n");
     printf("3) llenar vector en base a opcion 1 y 2.\n");
     printf("4) imprimir vectores.\n");
-    printf("5) llenar matriz 4 x 4");
+    printf("5) llenar matriz 4 x 4\n");
     printf("6) llenar matriz.\n");
-    printf("0) terminar programa.");
+    printf("0) terminar programa.\n");
     op= validar("elige una opcion\n",0,6);
 
     return op;
 }
 
-int validar(char[30], li, ls)
+int validar(char mensaje[30],int  li,int ls)
 {
-
+    int num;
+    printf("%s", mensaje);
+    scanf("%d",& num);
+    while(num<li || num>ls)
+    {
+        printf("opcion invalida. introduce una opcion entre %d y %d\n", li, ls);
+        scanf("%d",& num);
+    }
+    return num;
 }
 
 void vectorManual(int vector1[], int n)
 {
-    
+    int num;
+    for(int i=0; i<n; i++)
+    {
+        // printf( "dame un numero entre 30 y 70: \n");
+
+        vector1[i]= num=validar("dame un numero entre 30 y 70\n", 30, 70);
+    }
 }
 
 void vectorAleatorio(int vector2[], int n)
 {
-
+    srand(time(NULL));
+    for(int i=0; i<n; i++)
+    {
+        vector2[i]= (rand()% 20)+1;   
+    }
 }
 
-void vectores(int vector1[], int vector2[], int vector3[], int n, int n2)
+void vectores(int vector1[], int vector2[], int vector3[], int n)
 {
-
+    for(int i=0; i<n; i++)
+    {
+        vector3[i]= vector1[i];
+        vector3[i+n]= vector2[i];
+    }
 }
 
-void imprimirVectores(int vector1[], int vector2[], int vector3[], int n, int n2)
+void imprimirVectores(int *vector, int n)
 {
-
+    
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", vector[i]);
+    }
+    printf("\n");
 }
 
-void llenarMatriz(int vector1[], int vector2[], int matriz[][n2], int n, int n2)
+void llenarMatriz(int vector1[], int vector2[], int matriz[][3])
 {
-
+    int pos=0, i, j;
+    for( i=0; i<4; i++)
+    {
+        for( j=0; j<4; j++)
+        {
+            if(pos<10)
+            {
+                matriz[i][j]= vector1[pos];
+            }
+            else
+            {
+                matriz[i][j]= vector2[pos-10];
+            }
+            pos++;
+        }
+    }
 }
 
-void imprimirMattriz(int matriz[][n2], int n2)
+void imprimirMatriz(int matriz[][3], int m)
 {
-
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            printf("[%d] ", matriz[i][j], i, j);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
