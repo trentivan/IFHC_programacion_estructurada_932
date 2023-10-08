42e3dece7b5a41448fe6369a9127de22f29000b9
trentivan <141792967+trentivan@users.noreply.github.com>
2023-10-07 21:25:43 -0700

    modified:   actividad9/ivan.h 	modified:   actividad9/output/ivan.exe 	new file:   prueba/ivan.h 	new file:   prueba/prueba_libreria.c 	new file:   pruebas_de_funciones.c/aleatorioSinRepetir.c 	new file:   pruebas_de_funciones.c/output/aleatorioSinRepetir.exe

---
+++ prueba/prueba_libreria.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "ivan.h"
+
+int main()
+{
+    
+}
---
+++ pruebas_de_funciones.c/aleatorioSinRepetir.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void vectorSinRepetir(int vector[],int n, int li, int ls)
+{
+    srand(time(NULL));
+    int i, j;
+
+    for(i=0; i < n; i++)
+    {
+        int rango = ls-li+1;
+        vector[i]= (rand()% rango)+li;
+
+        for(j=0; j<i; j++)
+        {
+            if(vector[i]==vector[j])
+            {
+                i--;
+            }
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", vector[i]);
+    }
+}
