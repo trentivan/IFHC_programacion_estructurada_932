06a94021cf837d0e56ad5f5c1c2f363c3dcd530f
trentivan <141792967+trentivan@users.noreply.github.com>
2023-10-07 18:29:38 -0700

    new file:   pruebas_de_funciones.c/output/validacionTexto.exe 	new file:   pruebas_de_funciones.c/validacionTexto.c

---
+++ pruebas_de_funciones.c/validacionTexto.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+void validacionTexto(char cadena[], int n);
+
+int main()
+{
+    int n=100;
+    char cadena[n];
+    validacionTexto(cadena, n);
+    return 0;
+}
+
+void validacionTexto(char cadena[], int n)
+{
+    int invalido=0;
+    int i;
+
+    do{
+        invalido=0;
+        printf("Dame un texto\n");
+        fflush(stdin);
+        gets(cadena);
+
+        for(i=0 ; cadena[i] != '\0'; i++);
+        if(i > n)
+        {
+            printf("el texto escrito tiene mas de [%d] caracteres.\n", n);
+            invalido=1;
+        }
+
+        for(i=0 ; cadena[i] != '\0'; i++)
+        {
+            if(cadena[i] > 90)
+            {
+                if(cadena[i] >= 97)
+                {
+                    if(cadena[i] > 122)
+                    {
+                        invalido=1;
+                        printf("1) el texto no puede contener caracteres especiales como este que escribiste [%c]\n", cadena[i]);
+                    }
+                }
+                else
+                {
+                    invalido=1;
+                    printf("2) el texto no puede contener caracteres especiales como este que escribiste [%c]\n", cadena[i]);
+                }
+            }
+            else
+            {
+                if(cadena[i] == 32)
+                {
+                    if(cadena[i+1] == 32)
+                    {
+                        invalido=1;
+                        printf("3) el texto no puede contener dobles espacios\n");
+                    }
+                }
+                else
+                {
+                    if(cadena[i] < 65)
+                    {
+                        invalido=1;
+                        printf("4) el texto no puede contener caracteres especiales como este que escribiste [%c]\n", cadena[i]);
+                    }
+                }
+            }
+        }
+        if(invalido==1)
+        {
+            printf("Por favor escribe otro texto.\n");
+        }
+
+    }while(invalido == 1);
+    printf("%s", cadena);
+}
